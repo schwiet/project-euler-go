d1fe51bdc8c00ba5f2b6577290a0621fe561ec93
seth.schwiet <seth.schwiet@gmail.com>
2019-10-10 18:03:33 -0700

    decided to add problem statement to each solution file

--- solutions/p1-multiples-of-3-and-5.go
+++ solutions/p1-multiples-of-3-and-5.go
@@ -4,6 +4,13 @@ import (
     "fmt"
 )
 
+/*
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+    get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+ 
 func main(){
     sum := 0
     // sum all of the multiples of 3
