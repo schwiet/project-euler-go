3ede1373a7393d53caba116e4477e9caf22fe246
schwiet <seth.schwiet@pm.me>
2022-12-14 13:14:50 -0800

    added problem description to p16

--- solutions/p16-power-digit-sum.go
+++ solutions/p16-power-digit-sum.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+/*
+	215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+	What is the sum of the digits of the number 21000?
+*/
+
 func main() {
 	// TODO: use os.Args as input
 	// if len(os.Args) != 3 {
